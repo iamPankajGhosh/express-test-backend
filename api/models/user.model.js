ff9a96a230a71e4bbe5773bc5271dfb9a53a3a69
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-04-12 13:36:24 +0530

    refactor user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,11 +2,15 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "employee"], default: "employee" },
-});
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    employeeId: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "employee"], default: "employee" },
+  },
+  { timestamps: true }
+);
 
 export const User = model("User", userSchema);
